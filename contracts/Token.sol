a7689d6e830075d42cf0f23cc6b785f31d048356
Prabinbaral77 <prabin.baral00@gmail.com>
2022-06-15 15:49:18 +0545

    erc20 with mint and burn

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,18 +1,44 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract Token {
+    string public _name;
+    string public _symbol;
+    uint public _decimal;
+    uint public _totalSupply;
+    address public _minter;
 
-contract AhjinCoin is ERC20 {
-    constructor() ERC20("Ahjin Coin", "AC") {}
-    
-    function mint( uint256 amount) public payable {
-       require(msg.value == amount * 0.0001 ether, "Invalid amount of ether");
-       _mint(msg.sender, amount);
-   }
+    mapping(address => uint) balances;
 
-   receive() external payable{}
-   fallback() external payable{}
+    constructor() {
+        _symbol = "AC";
+        _name = "AJNIN COIN";
+        _decimal = 0;
+        _totalSupply = 100;
+        _minter = 0xEbb2B7eB0957362707b23e95B2883BFdef1Ce7f0;
+
+        balances[_minter] = _totalSupply;
+    }
+
+    event Transfer(address from, address to, uint256 value);
+
+    function balanceOf(address account) public view returns (uint256) {
+    return balances[account];
+    }
+
+    function mint(address account, uint amount) public returns(bool) {
+        balances[account] += amount;
+        _totalSupply += amount;
+        emit Transfer(msg.sender, account, amount);
+        return true;
+    }
+
+    function burn(address account, uint amount) public  {
+       uint accountBalances = balances[account];
+       require(accountBalances >= amount, "exceed burn amount");
+       balances[account] = accountBalances - amount;
+       _totalSupply -= amount;
+       emit Transfer(msg.sender, account, amount);
+    }
+
 }
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,9 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
-      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
+      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
     },
   },
 };
